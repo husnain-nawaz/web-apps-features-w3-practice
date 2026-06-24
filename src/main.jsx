6e3635e8b32f4e216868fc53e74791b0745eb465
husnain-nawaz <64336728+husnain-nawaz@users.noreply.github.com>
2026-06-24 05:57:40 +0500

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,110 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createPortal } from 'react-dom';
+
 import './index.css'
-import App from './App.jsx'
+import _App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <_App />
+  </StrictMode>,
+)
+
+
+
+
+
+// function Model({isopen,onclose,children}){
+//     if(!isopen){
+//         return null;
+//     }
+//     else{
+//     return(
+//         <div>
+//             {children}
+//             <button onClick={onclose}>close</button>
+//         </div>
+//     )}
+// }
+// function MyApp(){
+//     const [isopen, setisopen] = useState(false);
+//     return(
+//         <div>
+//             <button onClick={()=>setisopen(true)}>OPEN</button>
+//             <Model isopen={isopen} onclose={()=>setisopen(false)}>
+//                 <p>model content</p>
+//             </Model>
+//         </div>
+//     )
+// }
 
 // createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
+//   <MyApp />
 // )
 
 
 
 
 
-function Model({isopen,onclose,children}){
-    if(!isopen){
-        return null;
-    }
-    else{
-    return(
-        <div>
-            {children}
-            <button onClick={onclose}>close</button>
-        </div>
-    )}
-}
-function MyApp(){
-    const [isopen, setisopen] = useState(false);
-    return(
-        <div>
-            <button onClick={()=>setisopen(true)}>OPEN</button>
-            <Model isopen={isopen} onclose={()=>setisopen(false)}>
-                <p>model content</p>
-            </Model>
-        </div>
-    )
-}
+// function PortalButton({ onClick, children }) {
+//   return createPortal(
+//     <button onClick={onClick}>
+//       {children}
+//     </button>,
+//     document.body
+//   );
+// }
+// function App() {
+//   const [count1, setCount1] = useState(0);
+//   const [count2, setCount2] = useState(0);
+//   return (
+//     <div onClick={() => {
+//         setCount1(c => c + 1);
+//       }}>
+//       <h2>Div Clicked: {count1}</h2>
+//       <h2>Button Clicked: {count2}</h2>      
+//       <PortalButton
+//         onClick={(e) => {
+//           setCount2(c => c + 1);
+//         }}>
+//         Floating Button
+//       </PortalButton>
+//     </div>
+//   );
+// }
+// createRoot(document.getElementById('root')).render(
+//   <App />
+// );
 
-createRoot(document.getElementById('root')).render(
-  <MyApp />
-)
-
-
+// function PortalButton({onClick,children}){
+//   return createPortal(
+//     <button onClick={onClick}>
+//       {children}
+//     </button>,
+//     document.body
+//   )
+// }
+// function App(){
+//   const [count1, setcount1] = useState(0);
+//   const [count2, setCount2] = useState(0);
+//   return(
+//     <div onClick={()=>{
+//       setcount1(c=> c+1);
+//     }}>
+//       <h2>Div clicked: {count1}</h2>
+//       <h2>Button clicked: {count2}</h2>
+//     <PortalButton onClick={(e)=>{
+//       setCount2(c=>c+1)
+//     }}>
+//       Floating button
+//     </PortalButton>
+//     </div>
+//   )
+// }
+// createRoot(document.getElementById("root")).render(
+//   <App />
+// )
 
 
 
