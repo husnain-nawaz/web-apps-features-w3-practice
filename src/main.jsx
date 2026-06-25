6db2c811116364959682ef1b11325e05cb730c47
husnain-nawaz <64336728+husnain-nawaz@users.noreply.github.com>
2026-06-25 22:58:37 +0500

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -224,24 +224,176 @@ import _App from './App.jsx'
 
 
 
+// class Header extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {favoritecolor: "red"};
+//   }
+//   render(){
+//     return (
+//       <h1>My favourite Color is: {this.state.favoritecolor} </h1>
+//     )
+//   }
+// }
+// createRoot(document.getElementById('root')).render(
+//   <Header />
+// )
+
+
+
+
+// class Header extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {favoritecolor: "red"}
+//   }
+//   static getDerivedStateFromProps(props, state){
+//     return {favoritecolor: props.chnagecolor}
+//   }
+//   render(){
+//     return (
+//       <h1>My Favourite COLOR: {this.state.favoritecolor} </h1>
+//     )
+//   }
+// }
+// createRoot(document.getElementById('root')).render(
+//   <Header chnagecolor = "Purple" />
+// )
+
+
+
+// class Header extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {favouriteColor : "BLUE"}
+//   }
+//   componentDidMount(){
+//     setTimeout(()=>{
+//       this.setState({favouriteColor: "Yellowish"})
+//     },1000)
+//   }
+
+//   render(){
+//     return(
+//       <h1>MY Favourite Color is - {this.state.favouriteColor} </h1>
+//     )
+//   }
+// }
+// createRoot(document.getElementById('root')).render(
+//   <Header />
+// )
+
+
+
+
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       favoritecolor : "red",
+//       userchnaged : false,
+//     };
+//   }
+//   static getDerivedStateFromProps(props, state) {
+//     if(state.userchnaged){
+//       return null;
+//     }
+//     return {favoritecolor: props.favcol };
+//   }
+//   changeColor = () => {
+//     this.setState({
+//       favoritecolor: "blue",
+//       userchnaged : true,
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+//       <button type="button" onClick={this.changeColor}>Change color</button>
+//       </div>
+//     );
+//   }
+// }
+// createRoot(document.getElementById('root')).render(
+//   <Header favcol="yellow" />
+// );
+
+
+
+
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {favoritecolor: "red"};
+//   }
+//     shouldComponentUpdate(){ // ya automatic ture hota ha agr na b likahin ya part ha react ka or likh kr is ma fallse kiya to jo b ham changes krna chah rhy han ab nahi hon gy is ma
+//       return false;
+//     }
+//   changeColor = (()=>{
+//     this.setState({favoritecolor: "Blue"});
+//   })
+
+//   render() {
+//     return (
+//       <div>
+//       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+//       <button type="button" onClick={this.changeColor}>Change color</button>
+//       </div>
+//     );
+//   }
+// }
+// createRoot(document.getElementById('root')).render(
+//   <Header />
+// );
+
+
+
+
+// class Header extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {favoritecolor: "red"}
+//   }
+//   componentDidMount(){
+//     setTimeout(()=>{
+//       this.setState({favoritecolor: "Yellow"})
+//     },1000)
+//   }
+//   getSnapshotBeforeUpdate(prevprops, PrevState){
+//     document.getElementById("div1").innerHTML = "Before update was :" + PrevState.favoritecolor;
+//   }
+//   componentDidUpdate(){
+//     document.getElementById("div2").innerHTML = "Updated is " + this.state.favoritecolor;
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <h1>M favourit ecolor is {this.state.favoritecolor} </h1>
+//         <div id='div1'></div>
+//         <div id='div2'></div>
+//       </div>
+//     )
+//   }
+// }
+// createRoot(document.getElementById('root')).render(
+//   <Header />
+// );
+
+
+// function Car(myobj){ // ya jo myobj ha ya brand ha jo sab sy nich ha or us ki value ha FORD
+//   return(
+//     <h2>I am a {myobj.brand} </h2>
+//   )
+// }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+createRoot(document.getElementById('root')).render(
+  <Car brand="Ford" />
+)
 
 
 
